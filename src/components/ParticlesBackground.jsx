cc0ea88ce8e27bfcf9cddf769b7c5b7381cdba07
Khaled Hegazy <khaledhegazy870@gmail.com>
2023-10-01 16:39:27 +0300

    Decrease Particle Speed

--- src/components/ParticlesBackground.jsx
+++ src/components/ParticlesBackground.jsx
@@ -50,7 +50,7 @@ const ParticlesBackground = () => {
               default: "bounce",
             },
             random: false,
-            speed: 2,
+            speed: 1,
             straight: false,
           },
           number: {
