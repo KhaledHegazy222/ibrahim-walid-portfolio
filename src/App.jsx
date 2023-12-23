bde8aae2237bf306e9b38c260eb19aace62dd97c
Khaled Hegazy <khaledhegazy870@gmail.com>
2023-12-23 12:04:35 +0200

    Test BrowserRouter

--- src/App.jsx
+++ src/App.jsx
@@ -14,66 +14,68 @@ import Testimonials from "./components/Testimonials";
 import Tools from "./components/Tools";
 import { CssBaseline, ThemeProvider } from "@mui/material";
 import { theme } from "./theme";
-import { HashRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, HashRouter, Route, Routes } from "react-router-dom";
 import Details from "./components/Page2/Details";
 import Consultation from "./components/Page2/Consultation";
 import Extras from "./components/Page2/Extras";
 
 function App() {
   return (
-    <HashRouter basename="/ibrahim-walid-portfolio">
-      <ThemeProvider theme={theme}>
-        <CssBaseline />
-        <Routes>
-          <Route
-            path="/ui-design"
-            element={
-              <>
-                <Navbar fixed={false} />
-                <Details
-                  title={"تصميم شعارات"}
-                  content={
-                    "الآن يمكنك الوصول إلى قاعدة أكبر من العملاء عن طريق طلب خدمة تصميم مواقع الويب من شركة العالمية الحرة افضل شركة تصميم مواقع الكترونية في الوطن العربي، ساعد نشاطك التجاري على الانتشار بأفضل شكل ممكن من خلال تصميم مواقع ويب مميزة تعكس هويتك التجارية على الويب بصورة مثالية، يمكنك أيضا تصدر نتائج البحث عن طريق اعتمادك علينا في تصميم تصميم مواقع نت احترافية لنشاطك التجاري."
-                  }
-                />
-                <Consultation />
-                <Extras />
-                <Portfolio />
-                <Services />
-                <Testimonials />
-                <Footer />
-              </>
-            }
-          />
-          <Route
-            index
-            element={
-              <>
-                <StyledHeader>
-                  <Navbar />
-                  <Hero />
-                </StyledHeader>
-                <main>
-                  <About />
-                  <Services />
-                  <Numbers />
-                  <Skills />
-                  <Tools />
+    <BrowserRouter basename="/ibrahim-walid-portfolio">
+      <HashRouter>
+        <ThemeProvider theme={theme}>
+          <CssBaseline />
+          <Routes>
+            <Route
+              path="/ui-design"
+              element={
+                <>
+                  <Navbar fixed={false} />
+                  <Details
+                    title={"تصميم شعارات"}
+                    content={
+                      "الآن يمكنك الوصول إلى قاعدة أكبر من العملاء عن طريق طلب خدمة تصميم مواقع الويب من شركة العالمية الحرة افضل شركة تصميم مواقع الكترونية في الوطن العربي، ساعد نشاطك التجاري على الانتشار بأفضل شكل ممكن من خلال تصميم مواقع ويب مميزة تعكس هويتك التجارية على الويب بصورة مثالية، يمكنك أيضا تصدر نتائج البحث عن طريق اعتمادك علينا في تصميم تصميم مواقع نت احترافية لنشاطك التجاري."
+                    }
+                  />
+                  <Consultation />
+                  <Extras />
                   <Portfolio />
-                  <Clients />
+                  <Services />
                   <Testimonials />
-                  <div style={{ display: "none", color: "red" }}>
-                    <Contact />
-                  </div>
-                </main>
-                <Footer />
-              </>
-            }
-          />
-          <Route path="*" element={"Not Found"} />
-        </Routes>
-      </ThemeProvider>
-    </HashRouter>
+                  <Footer />
+                </>
+              }
+            />
+            <Route
+              index
+              element={
+                <>
+                  <StyledHeader>
+                    <Navbar />
+                    <Hero />
+                  </StyledHeader>
+                  <main>
+                    <About />
+                    <Services />
+                    <Numbers />
+                    <Skills />
+                    <Tools />
+                    <Portfolio />
+                    <Clients />
+                    <Testimonials />
+                    <div style={{ display: "none", color: "red" }}>
+                      <Contact />
+                    </div>
+                  </main>
+                  <Footer />
+                </>
+              }
+            />
+            <Route path="*" element={"Not Found"} />
+          </Routes>
+        </ThemeProvider>
+      </HashRouter>
+    </BrowserRouter>
   );
 }
 
